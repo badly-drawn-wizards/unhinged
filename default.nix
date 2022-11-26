7807f8640edb8817999c85f992914f1c0b35a788
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2022-11-27 00:43:41 +0200

    Add kmod

--- default.nix
+++ default.nix
@@ -1,7 +1,9 @@
-{ lib, pkgs, python3Packages }:
+{ lib, pkgs, python3Packages, kmod }:
 
 python3Packages.buildPythonPackage {
   pname = "unhinged";
   version = "0.0.1";
   src = ./.;
+
+  propagatedBuildInputs = [ kmod ];
 }
